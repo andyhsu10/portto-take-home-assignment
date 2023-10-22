db5a648e8f0d0e54385dcc3d7618cefd5aa5f730
An-Ting Hsu <andyhsu10@gmail.com>
2023-10-23 01:38:37 +0800

    feat: play around txn data and receipt logs

--- internal/app/indexer.go
+++ internal/app/indexer.go
@@ -2,11 +2,20 @@ package app
 
 import (
 	"context"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"math/big"
 
 	"eth-blockchain-service/internal/ethclient"
+
+	"github.com/ethereum/go-ethereum/core/types"
 )
+
+type ReceiptLog struct {
+	Index int    `json:"index"`
+	Data  string `json:"data"`
+}
 
 func Run() {
 	cl, err := ethclient.GetClient()
@@ -20,4 +29,25 @@ func Run() {
 	fmt.Println("Block", block, err)
 	hash, time, parentHash, transactions := block.Hash(), block.Time(), block.ParentHash(), block.Transactions()
 	fmt.Println("BlockInfo", hash, time, parentHash, transactions)
+
+	for _, t := range transactions {
+		hash := t.Hash()
+		receipt, _ := cl.TransactionReceipt(ctx, hash)
+		from, _ := types.Sender(types.LatestSignerForChainID(t.ChainId()), t)
+		to := t.To()
+		nonce := t.Nonce()
+		// FIXME: data might be empty
+		data := "0x" + hex.EncodeToString(t.Data())
+		value := t.Value()
+		logs := make([]*ReceiptLog, len(receipt.Logs))
+		for i, l := range receipt.Logs {
+			logs[i] = &ReceiptLog{
+				Index: int(l.Index),
+				Data:  "0x" + hex.EncodeToString(l.Data),
+			}
+		}
+		jsonLogs, _ := json.Marshal(logs)
+
+		fmt.Println("TXID:", hash, from.String(), to.String(), nonce, data, value, string(jsonLogs))
+	}
 }
